376bdea6264fd805f13ceb3856871dde6c7bed8b
Kaiirros <dgm6546@rit.edu>
2024-05-10 20:42:58 -0400

    Orbits

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { degToRad } from 'three/src/math/MathUtils.js';
 
 
 
@@ -14,18 +15,64 @@ if ( WebGL.isWebGLAvailable() ) {
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     const mouse = new THREE.Vector2();
-
     const renderer = new THREE.WebGLRenderer();
     const controls = new OrbitControls(camera, renderer.domElement);
-
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    const geometry = new THREE.BoxGeometry( 10, 10, 10 );
+
+    const floor = new THREE.CircleGeometry(200,16);
+    const floorMesh = new THREE.Mesh( floor );
+    floorMesh.rotateX(degToRad(-90))
+    scene.add(floorMesh)
+
+
+    const geometry = new THREE.SphereGeometry( 10, 16, 16 );
     const material = new THREE.MeshBasicMaterial( { color: 0x3187A2 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    const loader = new FontLoader();
-    loader.load(
+    const sphere = new THREE.Mesh( geometry, material );
+    sphere.translateZ(100)
+    scene.add( sphere );
+
+    const sphereTwo = new THREE.SphereGeometry( 10, 16, 16 );
+    const sphereTwoMat = new THREE.MeshBasicMaterial( { color: 0x00FF00 } );
+    const sphereTwoMesh = new THREE.Mesh( sphereTwo, sphereTwoMat );
+    sphereTwoMesh.translateZ(220)
+    scene.add( sphereTwoMesh );
+
+    const originGeo = new THREE.SphereGeometry(5, 16, 16)
+    const originMat = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+    const origin = new THREE.Mesh( originGeo, originMat );
+    scene.add(origin);
+
+
+camera.position.z = 80;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	sphere.rotation.y += 0.01;
+    sphere.translateX(1)
+
+    sphereTwoMesh.rotateY(0.009);
+    sphereTwoMesh.translateX(2)
+    console.log(sphereTwoMesh.rotation)
+    controls.update();
+
+
+	renderer.render( scene, camera );
+}
+
+animate();
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
+
+
+/*
+loader.load(
         // resource URL
         'node_modules/three/examples/fonts/helvetiker_bold.typeface.json',
 
@@ -61,25 +108,4 @@ if ( WebGL.isWebGLAvailable() ) {
         function ( err ) {
             console.log( 'An error happened' );
         }
-    );
-camera.position.z = 50;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-    controls.update();
-
-
-	renderer.render( scene, camera );
-}
-
-animate();
-
-} else {
-
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
-}
+    ); */
